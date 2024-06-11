9710bfc5d27af9a8c77f68bfa70c59733b86e39e
thanhle <thanhlche160808@fpt.edu.vn>
2024-06-11 16:08:17 +0700

    fix: refactor code

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -3,9 +3,9 @@ import cartRepository from "../repository/cart.repository.js";
 import productRepository from "../repository/product.repository.js";
 
 const cartService = {
-    add: async ({code, color, size, quantity}, account) => {
+    add: async ({ code, color, size, quantity }, account, cart) => {
         if (!account) {
-            const guestCart = cartService.addGuestCart(item);
+            const guestCart = cartService.addGuestCart({ code, color, size, quantity }, cart);
             return guestCart;
         }
 
@@ -34,22 +34,47 @@ const cartService = {
             accountId: account,
         });
     },
-    
-    addGuestCart: async (item) => {
+
+    addGuestCart: async ({ code, color, size, quantity }, cart) => {
+        const product = await productRepository.findProductByCode(code);
+
+        if (cart) {
+            const items = cart.items;
+            const existingItem = items.find(item => item.productCode === code && item.color === color && item.size === size);
+            if (existingItem) {
+                existingItem.quantity += quantity;
+                cart.totalPrice += product.price * quantity;
+            } else {
+                items.push({
+                    kind: "Product",
+                    productName: product.productName,
+                    productCode: product.productCode,
+                    color: color,
+                    size: size,
+                    quantity: quantity,
+                    price: product.price,
+                });
+                cart.totalPrice += product.price * quantity;
+            }
+            return {
+                items,
+                totalPrice: cart.totalPrice,
+            }
+        }
+
         let totalPrice = 0;
-        const product = await productRepository.findProductByCode(item.code);
         let cartItem = {};
         if (product) {
             cartItem = {
                 kind: "Product",
                 productName: product.productName,
                 productCode: product.productCode,
-                color: item.color,
-                size: item.size,
-                quantity: item.quantity,
+                color: color,
+                size: size,
+                quantity: quantity,
                 price: product.price,
             };
-            totalPrice += product.price * item.quantity;
+            totalPrice += product.price * quantity;
         }
 
         return {
@@ -90,6 +115,51 @@ const cartService = {
             items,
             totalPrice: userCart.totalPrice,
         }
+    },
+
+    updateCart: async ({ code, oldSize, oldQuantity, newSize, newQuantity }, cart, account) => {
+        //Guest cart
+        if (!account) {
+            return await cartService.handleUpdateItems({ code, oldSize, oldQuantity, newSize, newQuantity }, cart.items);
+        }
+
+        const userCart = await cartRepository.findCartByAccount(account);
+        const cartItems = JSON.parse(userCart.items);
+
+        const cartItemsUpdate = await cartService.handleUpdateItems({ code, oldSize, oldQuantity, newSize, newQuantity }, cartItems);
+        userCart.items = JSON.stringify(cartItemsUpdate.items);
+        userCart.totalPrice = cartItemsUpdate.totalPrice;
+        userCart.save();
+
+        return cartItemsUpdate
+    },
+
+    handleUpdateItems: async ({ code, oldSize, oldQuantity, newSize, newQuantity }, items) => {
+        const updateItems = [];
+        const index = items.findIndex(item => item.productCode === code && item.size === oldSize && item.quantity === oldQuantity);
+        items[index].size = newSize;
+        items[index].quantity = newQuantity;
+
+        // Merge duplicate products and update quantity
+        items.forEach(item => {
+            const existingItem = updateItems.find(updateItem => updateItem.productCode === item.productCode && updateItem.size === item.size);
+            if (existingItem) {
+                existingItem.quantity += item.quantity;
+            } else {
+                updateItems.push(item);
+            }
+        });
+
+        const totalPrice = await cartService.calculateTotalPrice(updateItems);
+        console.log('totalPrice:', totalPrice);
+        return {
+            items: updateItems,
+            totalPrice,
+        }
+    },
+
+    calculateTotalPrice: async (items) => {
+        return items.reduce((total, item) => total + (item.price * item.quantity), 0);
     }
 };
 
