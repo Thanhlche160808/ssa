1ea8c0476a90fcf876ddca9f04ec4f8e11364dad
thanhle <thanhlche160808@fpt.edu.vn>
2024-06-13 14:13:42 +0700

    fix: update pm2 app

--- src/routes/public/auth.js
+++ src/routes/public/auth.js
@@ -268,7 +268,7 @@ router.post("/google/login", authController.loginWithGoogle);
 /**
  * @swagger
  * /api/public/auth/refresh-access-token:
- *  post:
+ *  get:
  *     summary: refresh token
  *     tags: [Auth]
  *     responses:
@@ -292,6 +292,6 @@ router.post("/google/login", authController.loginWithGoogle);
  *                              type: string
  *                              example: eyJhbGciO....
  */
-router.post("/refresh-access-token", authController.refreshAccessToken);
+router.get("/refresh-access-token", authController.refreshAccessToken);
 
 export default router;
