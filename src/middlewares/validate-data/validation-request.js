df50d9b6cac0453d94ddb1b69f1f347031bb339a
thanhle <thanhlche160808@fpt.edu.vn>
2024-06-18 08:39:15 +0700

    fix: bug validate authentication

---
+++ src/middlewares/validate-data/validation-request.js
@@ -0,0 +1,29 @@
+// ** Libs
+import { validationResult } from "express-validator";
+
+// ** Utils
+import { response } from "../../utils/baseResponse.js";
+
+// ** Constants
+import { statusCode } from "../../constants/index.js";
+
+export const validate = (validations = []) => {
+  return async (req, res, next) => {
+    for (let validation of validations) {
+      const result = await validation.run(req);
+      if (result.errors.length) break;
+    }
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(statusCode.BAD_REQUEST).json(
+      response.error({
+        message: errors.array(),
+        code: statusCode.BAD_REQUEST,
+      })
+    );
+  };
+};
